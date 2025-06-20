cf056cce155eeebf143961e3f8845c7739917863
amanda955 <amanda.jayasingheappuhamilage@student.adelaide.edu.au>
2025-06-20 10:11:37 +0000

    [automatic commit] 2025-06-20 10:11:37 UTC 129.127.202.0

--- part1/app.js
+++ part1/app.js
@@ -10,9 +10,9 @@ const PORT = 8080;
 
 //  /api/dogs
 app.get('/api/dogs', (req, res) => {
-  const sql =
-    `SELECT d.name AS dog_name, d.size, u.username AS owner_username
-FROM Dogs d
+  const sql = `
+    SELECT d.name AS dog_name, d.size, u.username AS owner_username
+    FROM Dogs d
      JOIN Users u ON d.owner_id = u.user_id`;
   db.query('SELECT * FROM Dogs', (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
