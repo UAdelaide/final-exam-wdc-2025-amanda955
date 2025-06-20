c8f88895f0c29dfcf70bbf9107c794a82ccc986c
amanda955 <amanda.jayasingheappuhamilage@student.adelaide.edu.au>
2025-06-20 10:17:08 +0000

    [automatic commit] 2025-06-20 10:17:08 UTC 129.127.202.0

--- part1/app.js
+++ part1/app.js
@@ -29,7 +29,7 @@ app.get('/api/walkrequests/open', (req, res) => {
     JOIN Dogs d ON wr.dog_id = d.dog_id
     JOIN Users u ON d.owner_id = u.user_id
     WHERE wr.status = 'open';
-    `
+  `;
   db.query("SELECT * FROM WalkRequests WHERE status = 'open'", (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json(results);
