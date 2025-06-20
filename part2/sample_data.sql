c12c27a2dc5bc2230d33eb25e0d5a15b4bbe6667
amanda955 <amanda.jayasingheappuhamilage@student.adelaide.edu.au>
2025-06-20 14:21:57 +0000

    [automatic commit] 2025-06-20 14:21:56 UTC 122.150.234.5

--- part2/sample_data.sql
+++ part2/sample_data.sql
@@ -1,7 +1,7 @@
 -- Insert Users
 INSERT INTO Users (username, email, password_hash, role) VALUES
 ('amanda434', 'amanda@example.com', 'hashed123', 'owner'),
-('bobwalker', 'bob@example.com', 'hashed456', 'walker');
+('iresha623', 'iresha623@example.com', 'hashed456', 'walker');
 
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
