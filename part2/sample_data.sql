20b718a775eda32fd23a876e25c64e6025a643d3
amanda955 <amanda.jayasingheappuhamilage@student.adelaide.edu.au>
2025-06-20 14:22:40 +0000

    [automatic commit] 2025-06-20 14:22:40 UTC 122.150.234.208

--- part2/sample_data.sql
+++ part2/sample_data.sql
@@ -1,9 +1,9 @@
 -- Insert Users
 INSERT INTO Users (username, email, password_hash, role) VALUES
 ('amanda434', 'amanda@example.com', 'amandahash434', 'owner'),
-('iresha623', 'iresha623@example.com', 'ireshahash623', 'walker');
+('iresha623', 'iresha@example.com', 'ireshahash623', 'walker');
 
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
 ((SELECT user_id FROM Users WHERE username = 'amanda434'), 'Max', 'medium'),
-((SELECT user_id FROM Users WHERE username = 'amanda434'), 'Bella', 'small');
+((SELECT user_id FROM Users WHERE username = 'iresha623'), 'Bella', 'small');
