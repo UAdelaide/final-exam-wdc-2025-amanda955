fc692290b1e71aa8603cddf55e8061985574c530
amanda955 <amanda.jayasingheappuhamilage@student.adelaide.edu.au>
2025-06-21 06:27:00 +0000

    [automatic commit] 2025-06-21 06:27:00 UTC 122.150.234.208

--- part2/app.js
+++ part2/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
-
 require('dotenv').config();
 
 const app = express();
 
-// session managemnent
 const session = require('express-session');
 
 app.use(session({
@@ -17,16 +15,19 @@ app.use(session({
     }
 }));
 
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-// get dogs for owner
 const dogRoutes = require('./routes/dogRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/dogs', dogRoutes);
+
 
 // Export the app instead of listening here
 module.exports = app;
